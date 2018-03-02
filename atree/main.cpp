211345957002a9e8373172a4c34c6e637b71d5f2
hariprasad54 <31009432+hariprasad54@users.noreply.github.com>
2018-03-02 10:09:49 +0530

    Add files via upload

---
+++ atree/main.cpp
@@ -0,0 +1,329 @@
+#include<iostream>
+#include<stdlib.h>
+#include<stdio.h>
+using namespace std;
+
+class AVL
+{
+    struct node
+    {
+        int data;
+        node* left;
+        node* right;
+        int height;
+    };
+
+    node* root;
+
+    void makeEmpty(node* t)
+    {
+        if(t == NULL)
+            return;
+        makeEmpty(t->left);
+        makeEmpty(t->right);
+        delete t;
+    }
+
+    node* insert(int x, node* t)
+    {
+        if(t == NULL)
+        {
+            t = new node;
+            t->data = x;
+            t->height = 0;
+            t->left = t->right = NULL;
+        }
+        else if(x < t->data)
+        {
+            t->left = insert(x, t->left);
+            if(height(t->left) - height(t->right) == 2)
+            {
+                if(x < t->left->data)
+                    t = singleRightRotate(t);
+                else
+                    t = doubleRightRotate(t);
+            }
+        }
+        else if(x > t->data)
+        {
+            t->right = insert(x, t->right);
+            if(height(t->right) - height(t->left) == 2)
+            {
+                if(x > t->right->data)
+                    t = singleLeftRotate(t);
+                else
+                    t = doubleLeftRotate(t);
+            }
+        }
+
+        t->height = max(height(t->left), height(t->right))+1;
+        return t;
+    }
+
+    node* singleRightRotate(node* &t)
+    {
+        node* u = t->left;
+        t->left = u->right;
+        u->right = t;
+        t->height = max(height(t->left), height(t->right))+1;
+        u->height = max(height(u->left), t->height)+1;
+        return u;
+    }
+
+    node* singleLeftRotate(node* &t)
+    {
+        node* u = t->right;
+        t->right = u->left;
+        u->left = t;
+        t->height = max(height(t->left), height(t->right))+1;
+        u->height = max(height(t->right), t->height)+1 ;
+        return u;
+    }
+
+    node* doubleLeftRotate(node* &t)
+    {
+        t->right = singleRightRotate(t->right);
+        return singleLeftRotate(t);
+    }
+
+    node* doubleRightRotate(node* &t)
+    {
+        t->left = singleLeftRotate(t->left);
+        return singleRightRotate(t);
+    }
+
+    node* findMin(node* t)
+    {
+        if(t == NULL)
+            return NULL;
+        else if(t->left == NULL)
+            return t;
+        else
+            return findMin(t->left);
+    }
+
+    node* findMax(node* t)
+    {
+        if(t == NULL)
+            return NULL;
+        else if(t->right == NULL)
+            return t;
+        else
+            return findMax(t->right);
+    }
+
+    node* remove(int x, node* t)
+    {
+        node* temp;
+
+        // Element not found
+        if(t == NULL)
+            return NULL;
+
+        // Searching for element
+        else if(x < t->data)
+            t->left = remove(x, t->left);
+        else if(x > t->data)
+            t->right = remove(x, t->right);
+
+        // Element found
+        // With 2 children
+        else if(t->left && t->right)
+        {
+            temp = findMin(t->right);
+            t->data = temp->data;
+            t->right = remove(t->data, t->right);
+        }
+        // With one or zero child
+        else
+        {
+            temp = t;
+            if(t->left == NULL)
+                t = t->right;
+            else if(t->right == NULL)
+                t = t->left;
+            delete temp;
+        }
+        if(t == NULL)
+            return t;
+
+        t->height = max(height(t->left), height(t->right))+1;
+
+        // If node is unbalanced
+        // If left node is deleted, right case
+        if(height(t->left) - height(t->right) == 2)
+        {
+            // right right case
+            if(height(t->left->left) - height(t->left->right) == 1)
+                return singleLeftRotate(t);
+            // right left case
+            else
+                return doubleLeftRotate(t);
+        }
+        // If right node is deleted, left case
+        else if(height(t->right) - height(t->left) == 2)
+        {
+            // left left case
+            if(height(t->right->right) - height(t->right->left) == 1)
+                return singleRightRotate(t);
+            // left right case
+            else
+                return doubleRightRotate(t);
+        }
+        return t;
+    }
+
+    int height(node* t)
+    {
+        return (t == NULL ? -1 : t->height);
+    }
+
+    int getBalance(node* t)
+    {
+        if(t == NULL)
+            return 0;
+        else
+            return height(t->left) - height(t->right);
+    }
+
+    void inorder(node* t)
+    {
+        if(t == NULL)
+            return;
+        inorder(t->left);
+        cout << t->data << " ";
+        inorder(t->right);
+    }
+    void preorder(node *t)
+{
+    if (t == NULL)
+        return;
+    cout<<t->data<<"  ";
+    preorder (t->left);
+    preorder (t->right);
+
+}
+void postorder(node *t)
+{
+    if (t == NULL)
+        return;
+    postorder ( t ->left );
+    postorder ( t ->right );
+    cout<<t->data<<"  ";
+}
+void display_avl(node *ptr, int level)
+{
+    int i;
+    if (ptr!=NULL)
+    {
+        display_avl(ptr->right, level + 1);
+        printf("\n");
+        if (ptr == root)
+        cout<<"Root -> ";
+        for (i = 0; i < level && ptr != root; i++)
+            cout<<"        ";
+        cout<<ptr->data;
+        display_avl(ptr->left, level + 1);
+    }
+}
+
+
+public:
+    AVL()
+    {
+        root = NULL;
+    }
+
+    void insert(int x)
+    {
+        root = insert(x, root);
+    }
+
+    void remove(int x)
+    {
+        root = remove(x, root);
+    }
+
+    void display()
+    {
+        if (root == NULL)
+            {
+                cout<<"Tree is Empty"<<endl;
+                //continue;
+            }
+        display_avl(root,1);
+        cout << endl;
+    }
+    void in_order() {
+        inorder(root);
+        cout<<endl;
+    }
+    void pre_order() {
+        preorder(root);
+        cout<<endl;
+    }
+    void post_order() {
+        postorder(root);
+        cout<<endl;
+    }
+};
+
+int main()
+{
+    int choice,item;
+    AVL hl;
+    while (1)
+    {
+        cout<<"\n---------------------"<<endl;
+        cout<<"AVL Tree Implementation"<<endl;
+        cout<<"\n---------------------"<<endl;
+        cout<<"1.Insert Element into the tree"<<endl;
+        cout<<"2.Delete element"<<endl;
+        cout<<"3.Display Balanced AVL Tree"<<endl;
+        cout<<"4.InOrder traversal"<<endl;
+        cout<<"5.PreOrder traversal"<<endl;
+        cout<<"6.PostOrder traversal"<<endl;
+        cout<<"7.Exit"<<endl;
+        cout<<"Enter your Choice: ";
+        cin>>choice;
+        switch(choice)
+        {
+        case 1:
+            cout<<"Enter value to be inserted: ";
+            cin>>item;
+            hl.insert(item);
+            break;
+        case 2:
+            cout<<"enter value to be delete: ";
+            cin>>item;
+            hl.remove(item);
+            break;
+        case 3:
+            cout<<"Balanced AVL Tree:"<<endl;
+            hl.display();
+            break;
+        case 4:
+            cout<<"Inorder Traversal:"<<endl;
+            hl.in_order();
+            cout<<endl;
+            break;
+        case 5:
+            cout<<"Preorder Traversal:"<<endl;
+            hl.pre_order();
+            cout<<endl;
+            break;
+        case 6:
+            cout<<"Postorder Traversal:"<<endl;
+            hl.post_order();
+            cout<<endl;
+            break;
+        case 7:
+            exit(1);
+            break;
+        default:
+            cout<<"Wrong Choice"<<endl;
+        }
+    }
+    return 0;
+
+}
